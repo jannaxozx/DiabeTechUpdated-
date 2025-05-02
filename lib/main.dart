7593144fa50fe419d52acaad63508d6944e93b1e
jannaxozx <jannaflores180@gmail.com>
2025-05-02 15:09:54 +0800

    upload

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'DiabeTech App',
+      title: 'DiabeTech ',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
